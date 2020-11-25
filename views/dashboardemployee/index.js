62dff5d926518572207f0a9ace5ec3e3e253fddd
Hammad-23 <hsyed2633@gmail.com>
2020-11-25 13:52:20 +0500

    working

--- views/dashboardemployee/index.js
+++ views/dashboardemployee/index.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { StyleSheet, View, ScrollView, Image, TouchableOpacity,Text,ImageBackground } from "react-native";
-const image = { uri: 'https://reachttps://www.robertwalters.co.uk/content/dam/robert-walters/country/middle-east/images/webinars-banner/RW%20-%20Male%20Job%20Hunting%20Remotely%20-%20Colour%20Overlay%20Images%20-%20580x326px.jpgtjs.org/logo-og.pnghttps://st3.depositphotos.com/2898339/16096/i/1600/depositphotos_160964300-stock-photo-job-search-background.jpg' };
+
 function DashboardEmployee() {
   return (
     <ScrollView>
 
 
       <View style={styles.header}>
-      <ImageBackground source={image} style={styles.image}>
+      <ImageBackground source={require("../../assets/cover2.jpg")} style={styles.image}>
        
       
         {/* <Image
@@ -85,5 +85,6 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: 'cover',
     justifyContent: 'center',
+    
   }
 });
